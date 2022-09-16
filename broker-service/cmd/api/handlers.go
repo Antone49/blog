3934ddbff98f83a8fde235675a95f942a263334a
g.anthony.49@gmail.com <g.anthony.49@gmail.com>
2022-09-16 16:10:29 +0600

    Ajout mail service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -12,6 +12,7 @@ type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
 	Post   PostPayload `json:"post,omitempty"`
+	Mail   MailPayload `json:"mail,omitempty"`
 }
 
 type AuthPayload struct {
@@ -22,6 +23,12 @@ type AuthPayload struct {
 type PostPayload struct {
 	Id     int `json:"id,omitempty"`
 	Number int `json:"number,omitempty"`
+}
+
+type MailPayload struct {
+	Email 	string `json:"email"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -56,9 +63,68 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 		app.getLastestPosts(w, requestPayload.Post)
 	case "getPostTags":
 		app.getPostTags(w, requestPayload.Post)
+	case "mailContactUs":
+		app.mailContactUs(w, requestPayload.Mail)
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
+}
+
+func (app *Config) mailContactUs(w http.ResponseWriter, mail MailPayload) {
+	jsonData, _ := json.MarshalIndent(mail, "", "\t")
+
+	log.Printf("mailContactUs\n")
+
+	request, err := http.NewRequest("POST", "http://mail-service/send", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	var responsePayload jsonResponse
+	if err != nil {
+		log.Println(responsePayload.Message)
+	}
+
+	if response.StatusCode == http.StatusUnauthorized {
+		app.errorJSON(w, errors.New("invalid credentials"))
+		return
+	} else if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error call auth service"))
+		return
+	}
+
+	// create variable we will read response.Body
+	var jsonFromService jsonResponse
+
+	// decode
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if jsonFromService.Error {
+		app.errorJSON(w, err, http.StatusUnauthorized)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "mailContactUs!"
+	payload.Data = jsonFromService.Data
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+
+	log.Printf("mailContactUs end\n")
 }
 
 func (app *Config) getAllPosts(w http.ResponseWriter, a AuthPayload) {
